fa5adeb30e163a98ad228fe07ec27a1bda9a686b
manoj1440 <malviya.manoj1997@gmail.com>
2023-11-29 22:49:20 +0530

    Added error

--- routes/crateRoutes.js
+++ routes/crateRoutes.js
@@ -7,7 +7,7 @@ router.post('/', crateController.addCrate); // Create a new panel
 router.get('/', crateController.getCrates); // Get all panels
 router.get('/route', crateController.getCratesForRoutes); // Get all panels
 router.post('/bulk', crateController.bulkUploadCrates);
-router.get('/by/name', crateController.getCrateByName); // Get a panel by ID
+router.put('/by/name', crateController.getCrateByName); // Get a panel by ID
 router.get('/:id', crateController.getCrateById); // Get a panel by ID
 router.put('/:id', crateController.updateCrateById); // Update a panel by ID
 router.put('/', crateController.updateCrateByName); // Update a panel by ID
