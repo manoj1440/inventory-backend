ceffb5341ea7b94a635c0df3a6e8f5dc97527631
manoj1440 <malviya.manoj1997@gmail.com>
2023-11-29 21:43:20 +0530

    fixed crate by name

--- routes/crateRoutes.js
+++ routes/crateRoutes.js
@@ -7,8 +7,8 @@ router.post('/', crateController.addCrate); // Create a new panel
 router.get('/', crateController.getCrates); // Get all panels
 router.get('/route', crateController.getCratesForRoutes); // Get all panels
 router.post('/bulk', crateController.bulkUploadCrates);
+router.get('/by/name', crateController.getCrateByName); // Get a panel by ID
 router.get('/:id', crateController.getCrateById); // Get a panel by ID
-router.get('/byname', crateController.getCrateByName); // Get a panel by ID
 router.put('/:id', crateController.updateCrateById); // Update a panel by ID
 router.put('/', crateController.updateCrateByName); // Update a panel by ID
 router.delete('/:id', crateController.deleteCrateById); // Delete a panel by ID
